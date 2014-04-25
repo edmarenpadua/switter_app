350d9a047858cbcf16f58bddabdbf86fbc3e1e10
Mayen Padua <edmarenpadua@gmail.com>
2014-04-25 11:15:23 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwitterApp::Application.config.secret_key_base = '6b28dd837774fc8bea1abc40d4973a111920174639614f53ecfef31c75fdb9d9fb597d0fd7bf81ce5ed833a94588fcee4a3026f1d9927edffccee7bdb2af56c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwitterApp::Application.config.session_store :cookie_store, key: '_switter_app_session'
